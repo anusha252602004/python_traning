5f5838ec03b0531c7dd4f204b4a13560cdb6722a
anusha252602004 <anushatl2602@gmail.com>
2025-08-06 15:26:25 +0530

    Add files via upload

--- mysql_conn.py
+++ mysql_conn.py
@@ -1,21 +1,21 @@
 import mysql.connector
-def insert_data(id,name,email):
+def update_people(people_id,new_name,password):
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
         password="roottoor",
         database="anusha_ece"
-        )
+    )
     mycursor = mydb.cursor()
-    sql = "INSERT INTO user (id,name,email)VALUES(%s,%s,%s)"
-    val = [id,name,email]
+    sql = "UPDATE people SET name = %s,password = %s WHERE id = %s"
+    val = (new_name,password,people_id)
     mycursor.execute(sql,val)
-    mydb.commit()
+    mydb.commit()   
     mycursor.close()
     mydb.close()
     print(mycursor.rowcount, "record inserted.")
 
-id = input("enter the id")
-name = input("enter the name")
-email = input("enter the email")
-insert_data(id,name,email)
+id = input("enter people ID to update ")
+name = input("enter new name")
+password = input("enter new password")
+update_people(id,name,password)
